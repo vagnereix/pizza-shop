7b7a3889683814a8883a601f41da72fb2549045d
Vagmer Reis <vagnereix.dev@gmail.com>
2024-01-17 23:46:15 -0300

    feat: add sign-in with react-query and axios

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,12 @@
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useMutation } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
+import { signIn } from '@/api/sign-in'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
@@ -25,19 +27,28 @@ export function SignIn() {
     resolver: zodResolver(signInFormSchema),
   })
 
+  const { mutateAsync: authenticate } = useMutation({
+    mutationFn: signIn,
+  })
+
   async function handleSignIn(data: SignInFormValues) {
-    console.log(data)
+    try {
+      authenticate({
+        email: data.email,
+      })
 
-    await new Promise((resolve) => setTimeout(resolve, 1000))
+      toast.success('Enviamos um link de autenticação para seu e-mail.', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => handleSignIn(data),
+        },
+      })
 
-    toast.success('Enviamos um link de autenticação para seu e-mail.', {
-      action: {
-        label: 'Reenviar',
-        onClick: () => handleSignIn(data),
-      },
-    })
-
-    reset()
+      reset()
+    } catch (error) {
+      console.log(error)
+      toast.error('Credenciais inválidas.')
+    }
   }
 
   return (
